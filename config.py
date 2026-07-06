11c6bde2df0b783d4e427546b21f74c99c51962c
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-06 10:56:29 +0100

    remove n flag

--- config.py
+++ config.py
@@ -8,9 +8,8 @@ print('Checking apictl installation')
 subprocess.run(['apictl', 'version'])
 
 print('Setting up test environment')
-
-subprocess.run(['apictl', 'remove', 'env', '-n', 'local'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-subprocess.run(['apictl', 'add', 'env', '-n', 'local', '-apim', 'https://localhost:9443'])
+subprocess.run(['apictl', 'remove', 'env', 'local'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+subprocess.run(['apictl', 'add', 'env', 'local', '-apim', 'https://localhost:9443'])
 
 print('Logging into local')
 
