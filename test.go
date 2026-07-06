3571120885864036cea9a3dcb2abf48073bc6b11
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-06 16:39:26 +0100

    fetch more env variables

--- test.go
+++ test.go
@@ -9,6 +9,8 @@ import (
 )
 
 var baseUrl string
+var username string
+var password string
 
 func main() {
 
@@ -19,13 +21,13 @@ fmt.Println(baseUrl)
 }
 
 func setENV() {
-
 //	Open the .env file
 envFile, err := os.Open("./.env")
 //	check for errors
 if err !=  nil {
 log.Fatalln(err)
 }
+
  //	defer closing the file until the function exits
 defer envFile.Close()
 
@@ -37,14 +39,14 @@ for scanner.Scan() {
 //	split the text on the equal sign to get the key and value
 envVar := strings.Split(scanner.Text(), "=")
 //	os.Setenv takes in a key and a value which are both strings
-
 os.Setenv(envVar[0], envVar[1])
 }
 //	check for errors with scanner.Scan
 if err := scanner.Err(); err !=  nil {
 log.Fatal(err)
 }
-
 // assign the environment variable using the os.Getenv method
 baseUrl = os.Getenv("BASE_URL")
+username = os.Getenv("USERNAME")
+password = os.Getenv("PASSWORD")
 }
