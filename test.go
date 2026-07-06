c8f9556b4281d1cb430d9a853cbd9c9ff07273d0
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-06 16:21:32 +0100

    change code to go from python

---
+++ test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+"bufio"
+"fmt"
+"log"
+"os"
+"strings"
+)
+
+var baseUrl string
+
+func main() {
+
+setENV()
+
+fmt.Println(baseUrl)
+
+}
+
+func  SetENV() {
+
+//	Open the .env file
+envFile, err := os.Open("./.env")
+//	check for errors
+if err !=  nil {
+log.Fatalln(err)
+}
+ //	defer closing the file until the function exits
+defer envFile.Close()
+
+// create a new scanner to read each row
+scanner := bufio.NewScanner(envFile)
+
+//	 loop through each row of the .env file
+for scanner.Scan() {
+//	split the text on the equal sign to get the key and value
+envVar := strings.Split(scanner.Text(), "=")
+//	os.Setenv takes in a key and a value which are both strings
+
+os.Setenv(envVar[0], envVar[1])
+}
+//	check for errors with scanner.Scan
+if err := scanner.Err(); err !=  nil {
+log.Fatal(err)
+}
+
+// assign the environment variable using the os.Getenv method
+baseUrl = os.Getenv("BASE_URL")
+}
--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv 
-load_dotenv()
-
-# Environment variables. Change it to the desired URL in .env file
-base_url = os.getenv('BASE_URL')
