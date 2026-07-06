03f629965cffa982b94f7ba17552a97857491d97
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-06 11:11:31 +0100

    create new python script to update policies

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 
+
+
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
 
@@ -12,5 +14,4 @@ subprocess.run(['apictl', 'remove', 'env', 'local'], stderr=subprocess.DEVNULL, 
 subprocess.run(['apictl', 'add', 'env', 'local', '--apim', 'https://localhost:9443'])
 
 print('Logging into local')
-
 subprocess.run(['apictl', 'login', 'local', '-u', username, '-p', password, '-k'])
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+
+# Environment variables. Change it to the desired URL.
+base_url = 'https://localhost:9443'
