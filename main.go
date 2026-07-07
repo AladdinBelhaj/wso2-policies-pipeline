829ebaa544d65c4a93e8b3d4e54f29198e0b2f9b
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-07 08:18:56 +0100

    comment the main.go file

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// This file executes a curl command to fetch APIs from the WSO2 API Manager using environment variables for authentication.
 package main
 
 import (
@@ -7,11 +8,13 @@ import (
 	"wso2/scripts/vars"
 )
 func main() {
-
+// Load the environment variables from the .env file
 	vars.Load()
-
+// Create a new command to execute the curl command with the environment variables
     cmd := exec.Command("curl", "-u", vars.Username + ":" + vars.Password, vars.BaseUrl + "/apis", "-k")
+// Execute the command and capture the output
 	out, err := cmd.Output()
+// Check for errors and log them if any
 	if err != nil {
 		log.Fatal(err)
 	}
