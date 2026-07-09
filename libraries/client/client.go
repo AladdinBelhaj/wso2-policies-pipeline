d4a290768fa27570355605c784dff5c126ff77ec
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-09 08:51:03 +0100

    pull id of first revision if revisions are 5

--- libraries/client/client.go
+++ libraries/client/client.go
@@ -171,7 +171,7 @@ func PutApiUpdate(apiId string, payload []byte) error {
 }
 
 
-func ReviewRevisionsNumber(apiId string) bool {
+func ReviewRevisionsNumber(apiId string) {
 	cmd := exec.Command("curl", "-u", vars.Username+":"+vars.Password, vars.BaseUrl+ "/apis/" + apiId + "/revisions", "-k")
 	jsonObject, err := cmd.Output()
 	if err != nil {
@@ -182,12 +182,20 @@ func ReviewRevisionsNumber(apiId string) bool {
 
 	json.Unmarshal(jsonObject, &data)
 
-	count := data["count"].(float64)
+	// count := data["count"].(float64)
 
-	if count == 5 {
-		return true
-	} else{
-		return false
-	}
+	// if count == 5 {
+	// 	return true
+	// } else{
+	// 	return false
+	// }
+
+	list := data["list"].([]interface{})
+	fmt.Println(list[0].(map[string]interface{})["id"])
 }
+
+
+// func deleteRevision(apiId string, revisionId string) error {
+
+// }
 
--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"wso2/scripts/libraries/client"
 	// "wso2/scripts/libraries/policy"
 	"wso2/scripts/vars"
-	"fmt"
+	// "fmt"
 )
 
 func main() {
 	vars.Load()
 
 	// apiIds := client.ExtractApiIds()
-	trueOrFalse := client.ReviewRevisionsNumber("2748d335-8e67-4724-b04d-6e6e13a32415")
-	fmt.Println(trueOrFalse)
+	client.ReviewRevisionsNumber("2748d335-8e67-4724-b04d-6e6e13a32415")
 	// apiPolicies := client.ExtractApiPolicies(apiIds)
 	// allPolicies := client.ExtractOperationPolicies()
 
