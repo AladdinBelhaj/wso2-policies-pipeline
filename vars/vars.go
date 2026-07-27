999061d5dce8c00b8a284c3753b3463607ebd9b9
AladdinBelhaj <alabelhaj2002@gmail.com>
2026-07-27 10:41:09 +0100

    use library to fetch env variables to avoid special characters issue in variables

--- vars/vars.go
+++ vars/vars.go
@@ -1,10 +1,10 @@
-// This file fetches environment variables from the .env file and assigns them to global variables for use in the application.
 package vars
 
 import (
-	"bufio"
+	"log"
 	"os"
-	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 var BaseURL string
@@ -13,23 +13,11 @@ var Password string
 var Vhost string
 
 func Load() {
-	// Open the .env file if it exists
-	if envFile, err := os.Open("./.env"); err == nil {
-		defer envFile.Close()
-		scanner := bufio.NewScanner(envFile)
-		for scanner.Scan() {
-			line := strings.TrimSpace(scanner.Text())
-			if line == "" || strings.HasPrefix(line, "#") {
-				continue
-			}
-			envVar := strings.SplitN(line, "=", 2)
-			if len(envVar) == 2 {
-				os.Setenv(envVar[0], envVar[1])
-			}
-		}
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
 	}
 
-	// assign environment variables
 	BaseURL = os.Getenv("BASE_URL")
 	Username = os.Getenv("USERNAME")
 	Password = os.Getenv("PASSWORD")
